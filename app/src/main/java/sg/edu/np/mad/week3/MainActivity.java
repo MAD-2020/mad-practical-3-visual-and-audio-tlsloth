1590c867f7f5e329376386c1cbdf63c47dafd8b2
Zhe Wei Benjamin Lai <s10193638@connect.np.edu.sg>
2020-05-06 10:25:59 +0800

    Added files and code to completion

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,13 +2,19 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView view = findViewById(R.id.rabbidVideo);
+        view.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.rabbid));
+        view.start();
     }
 }
